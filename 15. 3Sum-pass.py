d7473a2b1ddc909c03e34329f4c52d3856380112
ViewV <zxn@zxnnet.top>
2018-02-26 21:19:58 +0800

    Add even more commit to 15.3Sum**important**

--- 15. 3Sum-pass.py
+++ 15. 3Sum-pass.py
@@ -5,16 +5,21 @@
 class Solution:
     def threeSum(self, nums):
         res = []
-        nums.sort()
+        nums.sort()#very import to sort
         for i in range(len(nums)-2):
+            #to avoid special input
+            #if length of nums is less then 3
+            #this for block will not work
             if i > 0 and nums[i] == nums[i-1]:
-                #to avoid special input
                 continue
+            #i must from 1 to end
+            #because i<r<l to achieve binery search
             l, r = i+1, len(nums)-1#a special thing in python
             while l < r:
                 s = nums[i] + nums[l] + nums[r]
                 #remmber we have been sorted the list
-                if s < 0:#s<0 means that we need to make the l bigger
+                if s < 0:
+                    #s<0 means that we need to make the l bigger
                     l +=1 #l is left
                 elif s > 0:
                     r -= 1
