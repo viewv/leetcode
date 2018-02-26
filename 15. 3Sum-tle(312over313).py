4ecb97f0e55f8d8603f1315ae68caa71fd358606
ViewV <zxn@zxnnet.top>
2018-02-26 20:40:36 +0800

    A tle version 312 over 313 but in fact I don't finish my own edition

---
+++ 15. 3Sum-tle(312over313).py
@@ -0,0 +1,36 @@
+#copy from
+#https://leetcode.com/problems/3sum/discuss/7393/
+#Straight-forward-Python-AC-O(n2)-
+#solution-with-decent-explanation
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    def threeSum(self, nums):
+        if len(nums) <3: # deal with special input
+            return []
+        elif len(nums) == 3:
+            if sum(nums) == 0:
+                return [sorted(nums)]
+
+        nums = sorted(nums) # sorted, O(nlgn)
+        ans = []
+
+        for i in range(len(nums) -2):
+            j = i+1
+            k = len(nums) -1 # hence i < j < k
+
+            while j<k: # if not cross line
+                temp_sum = nums[i] + nums[j] + nums[k]
+                if temp_sum == 0:
+                    ans.append((nums[i], nums[j], nums[k]))
+
+                if temp_sum > 0: 
+                    # which means we need smaller sum, 
+                    # move k backward, remember we sort the array
+                    k -= 1
+                else:
+                    j += 1
+
+        return list(set(tuple(ans))) 
+        # I bet this is not the best way to 
+        # eliminate duplicate solutions
