13a066ff5c580451a38b0c62815dca94426d48f7
ViewV <zxn@zxnnet.top>
2018-02-26 11:41:18 +0800

    I don't  want do 13 :(

---
+++ 13. Roman to Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {  
+//copy from Internet
+//http://blog.csdn.net/feliciafay/article/details/17259547
+public:  
+    int romanToInt(std::string s) {  
+        int sum = 0;  
+        int start = 0;  
+        char char_arr[] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};  
+        int int_arr[] = {1, 5, 10, 50, 100, 500, 1000};  
+        std::map<char, int> roman_map;  
+        int map_len = sizeof(char_arr)/sizeof(char);   
+        for (int i = 0; i< map_len; ++i)   
+            roman_map.insert(std::pair<char, int> (char_arr[i], int_arr[i]));  
+        for (int i = 0; i < s.length() - 1; ++i) {  
+            if (roman_map[s[i]]>=roman_map[s[i + 1]])  
+                sum += roman_map[s[i]];  
+            else  
+                sum -= roman_map[s[i]];  
+        }  
+        sum += roman_map[s[s.length()-1]];  
+        return sum;  
+    }  
+}; 
