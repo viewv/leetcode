0bdcab8e1069df921c66a76051a00c4a505008a4
ViewV <zxn@zxnnet.top>
2018-02-26 21:06:31 +0800

    add more commit to 15.3Sum**important**

--- 15. 3Sum-pass.py
+++ 15. 3Sum-pass.py
@@ -1,15 +1,16 @@
 #copy from https://leetcode.com/problems/3sum/discuss
 # /7392/Python-easy-to-understand-solution-(O(n*n)-time).
 #a very good code!
-#I need to rember
+#I need to learn this code
 class Solution:
     def threeSum(self, nums):
         res = []
         nums.sort()
         for i in range(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
+                #to avoid special input
                 continue
-            l, r = i+1, len(nums)-1
+            l, r = i+1, len(nums)-1#a special thing in python
             while l < r:
                 s = nums[i] + nums[l] + nums[r]
                 #remmber we have been sorted the list
@@ -20,9 +21,14 @@ class Solution:
                 else:
                     res.append((nums[i], nums[l], nums[r]))
                     #blow code is a way to avoid the duplicate numbers
+                    #l and r have been add ro the finall anwser
+                    #the nums has been sorted so we just have to check
+                    #the number nearby
                     while l < r and nums[l] == nums[l+1]:
                         l += 1
                     while l < r and nums[r] == nums[r-1]:
                         r -= 1
+                    #because l and r have changed in to l=l-1
+                    #r=r+1, so we must make it correct
                     l += 1; r -= 1
         return res
