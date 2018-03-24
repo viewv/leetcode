b5c305a1dc51f0fcf6f6d89b6dbbe40b11257566
ViewV <zxn@zxnnet.top>
2018-03-24 21:24:11 +0800

    I don't want to do

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,3 +1,10 @@
 class Solution:
     def isValid(self, s):
-        pass#emmmm
+        dic = {"()", "[]", "{}"}
+        arr = []
+        for i in range(0, len(s)):
+            if len(arr) != 0 and (arr[-1]+s[i]) in dic:
+                arr.pop()
+            else:
+                arr.append(s[i])
+        return len(arr) == 0
