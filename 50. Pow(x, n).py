f3f1483675287e9b397ff0c0f3efda41a94b32cf
viewv <zxn@zxnnet.top>
2018-06-12 17:33:06 +0800

    chang 50 to fast onr

--- 50. Pow(x, n).py
+++ 50. Pow(x, n).py
@@ -1,3 +1,16 @@
 class Solution:
     def myPow(self, x, n):
-        return x**n
+        y = 1
+        while True:
+            t = n % 2
+            n = n // 2
+            if t == 1:
+                y = y * x
+            if n == 0:
+                break
+            x = x * x
+        return y
+
+solobj = Solution()
+
+print(solobj.myPow(2,10))
