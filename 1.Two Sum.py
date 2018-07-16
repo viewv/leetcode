051cc57203ea6015846d48eb4dabb1afa3e5974f
Zhang XueNan <zxn@zxnnet.top>
2018-07-16 16:03:18 +0800

    change vscode font

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -6,7 +6,7 @@ class Solution:
             a=nums[x]
             for y in range(x+1,length):
                 b=nums[y]
-                if a+b==target:
+                if a+b == target:
                     ans[0]=x
                     ans[1]=y
                     return ans
