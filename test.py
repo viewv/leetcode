935e62c62607e8c5100da5fb3b9d59b40c28fcfc
Zhang XueNan <zxn@zxnnet.top>
2018-07-20 14:25:36 +0800

    update 54 solution

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
-num = [2, 3, 1, 1, 4]
-
-def maxforword(a, nums):
-    step = nums[a]
-    return max(nums[a + 1:a+step])
-
-
-def jump(nums):
+def spiralOrder(matrix):
     x = 0
+    y = 0
+    point = 0
     count = 0
-    Length = len(nums)
-    while x + maxforword(x, nums) <= Length:
-        for y in range(x + 1, x + nums[x]):
-            if y == maxforword(x, nums):
-                x = y
-                break
+    ans = []
+    xdistion = [1, 0, -1, 0]
+    ydistion = [0, 1, 0, -1]
+    h = len(matrix)
+    l = len(matrix[0])
+    xdis = 0
+    ydis = 0
+    size = h * l
+    while count < size:
+        ans.append(matrix[y][x])
+        xdis = xdistion[point]
+        ydis = ydistion[point]
+        matrix[y][x] = None
         count += 1
-    return count
+        x = x + xdis
+        y = y + ydis
+        if x + xdis >= l or y + ydis >= h or x + xdis < 0 or y + ydis < 0:
+            point += 1
+            point = point % 4
+        elif matrix[y + ydis][x + xdis] == None:
+            point += 1
+            point = point % 4
+        else:
+            pass
+    return ans
 
 
-print(jump(num))
+matrix = [[3], [2]]
+
+ans = spiralOrder(matrix)
+print(ans)
