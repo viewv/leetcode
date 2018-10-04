304e5bfde8f545c6746254d5c9d0b0627ca1fd8b
viewv <zxn@zxnnet.top>
2018-10-04 18:27:36 +0800

    I infact find this algor but I write it wrong I put the end = 1 :(

---
+++ 167. Two Sum II - Input array is sorted(On).py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        length = len(numbers)
+        start, end = 0, length-1
+        while True:
+            if numbers[start] + numbers[end] == target:
+                return [start + 1, end + 1]
+            elif numbers[start] + numbers[end] > target:
+                end -= 1
+            else:
+                start += 1
