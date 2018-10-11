f70c29c4f13a81a8286a17b2ca627849db84026c
viewv <zxn@zxnnet.top>
2018-10-11 15:10:29 +0800

    It is too sensitive to the format problem

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def twoSum(self, nums, target):
-        length=len(nums)
-        ans=[0,0]
-        for x in range(0,length):
-            a=nums[x]
-            for y in range(x+1,length):
-                b=nums[y]
+        length = len(nums)
+        ans = [0, 0]
+        for x in range(0, length):
+            a = nums[x]
+            for y in range(x+1, length):
+                b = nums[y]
                 if a+b == target:
-                    ans[0]=x
-                    ans[1]=y
-                    return ans 
+                    ans[0] = x
+                    ans[1] = y
+                    return ans
