2713a6807f246ae52d235224e64d7d764683dbf8
Zhang XueNan <zxn@zxnnet.top>
2019-01-16 15:25:51 +0800

    dp for 198

---
+++ 198. House Robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        length = len(nums)
+        if length == 0:
+            return 0
+        if length == 1:
+            return nums[0]
+        dp = [0 for x in range(0, length)]
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, length):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[-1]
