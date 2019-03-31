ef4f223e0bce0a7a698eb72d059b56bc9075bc1d
viewv <zxn@zxnnet.top>
2019-03-31 15:43:33 +0800

    I am test new computer

--- 4. Median of Two Sorted Arrays.py
+++ 4. Median of Two Sorted Arrays.py
@@ -1,11 +1,11 @@
 class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
-        num=nums1+nums2
-        num=sorted(num)
-        n=len(num)
-        if n%2==0:
-            mid=n//2
+        num = nums1+nums2
+        num = sorted(num)
+        n = len(num)
+        if n % 2 == 0:
+            mid = n//2
             return (num[mid-1]+num[mid])/2
         else:
-            mid=(n+1)//2
+            mid = (n+1)//2
             return num[mid-1]
