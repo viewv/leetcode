812c8a719ec77f29f65b1ed7a742989b2d6e3409
ZhangXuenan <zxnnet@gmail.com>
2019-08-14 17:00:44 +0800

    果然暴力tle了1014

---
+++ 1014. Best Sightseeing Pair.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxScoreSightseeingPair(self, A: List[int]) -> int:
+        length = len(A)
+        lplu = [0 for x in range(length)]
+        lmin = [0 for x in range(length)]
+        maxnum = 0
+        for x in range(0, length):
+            lplu[x] = A[x] + x
+            lmin[x] = A[x] - x
+        for x in range(length - 1):
+            start = lplu[x]
+            end = max(lmin[x + 1:])
+            if start + end >= maxnum:
+                maxnum = start + end
+        return maxnum
