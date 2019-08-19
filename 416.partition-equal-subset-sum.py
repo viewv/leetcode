1a5d9551038c2bbb290ede0b5cc61f23dff3e11e
ZhangXuenan <zxnnet@gmail.com>
2019-08-19 18:09:23 +0800

    416我觉得是子集和问题

--- 416.partition-equal-subset-sum.py
+++ 416.partition-equal-subset-sum.py
@@ -1,4 +1,8 @@
-from typing import List
+#
+# @lc app=leetcode id=416 lang=python3
+#
+# [416] Partition Equal Subset Sum
+#
 
 
 class Solution:
@@ -11,23 +15,21 @@ class Solution:
         row = len(nums) + 1
         col = part + 1
         s = [[0 for c in range(col)] for r in range(row)]
-        s[0][0] = 1
         for i in range(1, col):
             s[0][i] = 0
         for j in range(1, row):
             s[j][0] = 1
             for k in range(1, col):
                 s[j][k] = s[j - 1][k]
-                if s[j][k] == 1:
-                    s[j][k] = s[j][k]
-                elif (k - nums[j - 1] >= 0) and (s[j-1][k - nums[j - 1]] == 1):
-                    s[j][k] = s[j-1][k - nums[j - 1]]
-                else:
-                    s[j][k] = 0
+                if s[j][k] != 1 and (k - nums[j - 1] >= 0) and (s[j - 1][k - nums[j - 1]] == 1):
+                    s[j][k] = s[j - 1][k - nums[j - 1]]
                 if k == col - 1 and s[j][k] == 1:
                     return True
         return False
 
-
-sol = Solution()
-print(sol.canPartition([1, 2, 5]))
+# 子集和问题
+# dp[i,w] = dp[i-1,w]||dp[i-1][w-nums[i]]
+# dp[i,w] 代表在前i个元素中是否存在子集和等于w
+# 第一种情况在i之前就有了，不选择i，有dp[i-1,w]
+# 第二种情况在i之前还没有，选择i，测试dp[i-1,w-nums[i]]
+# 测试是否i前面的子集存在能够不出残缺的集合，最后求或得解
