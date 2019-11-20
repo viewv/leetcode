6885069663efd7d0e6761890c00f1f0e214ecad3
= <=>
2019-11-20 21:37:35 +0800

    add some problem

--- Hashtable/1124. Longest Well-Performing Interval.py
+++ Hashtable/1124. Longest Well-Performing Interval.py
@@ -1,6 +1,3 @@
-from typing import List
-
-
 class Solution:
     def longestWPI(self, hours: List[int]) -> int:
         res = dict()
@@ -15,7 +12,3 @@ class Solution:
             if s - 1 in res:
                 ans = max(ans, i-res[s-1])
         return ans
-
-
-sol = Solution()
-print(sol.longestWPI([9, 9, 6, 0, 6, 6, 9]))
